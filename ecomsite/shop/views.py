82b9e0164cc60841ec94c962ebdb1b89f56ac504
sajjad <sajjadzolghadr777@gmail.com>
2026-07-16 12:51:21 +0330

    feat: implement product search feature

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Product
 # Create your views here.
 def index(request):
     products = Product.objects.all()
+    search = request.GET.get("search")
+
+    if search:
+        products = products.filter(
+            Q(title__icontains=search) |
+            Q(category__icontains=search) |
+            Q(description__icontains=search)
+        )
     return render(request,'shop/index.html',{'products':products})
