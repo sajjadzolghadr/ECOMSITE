1cbfde30a394e19a47e648f1ab7864d375c6701a
sajjad <sajjadzolghadr777@gmail.com>
2026-07-18 13:29:25 +0330

    feat: create order and order item models for checkout

--- ecomsite/shop/models.py
+++ ecomsite/shop/models.py
@@ -11,3 +11,18 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order #{self.id}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE,related_name="items")
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.product.title
